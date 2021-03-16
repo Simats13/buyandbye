229f82ac1dd411e86a0d5934813928051b275bb9
Mathis Maximin <m.maximin@intech-sud.fr>
2021-03-16 20:00:57 +0100

    suppression

--- lib/templates/oficihome_app_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class OficihomeAppTheme {
-  OficihomeAppTheme._();
-  static const Color orange = Color(0xFFFF8C00);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color white_grey = Color(0xFFC7C7CA);
-  static const Color black_electrik = Color(0xFF131313);
-}
