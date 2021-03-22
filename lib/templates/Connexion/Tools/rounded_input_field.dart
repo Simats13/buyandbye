9208e7af3d44d5a460394530086bf300445e27db
Simats13 <39007520+Simats13@users.noreply.github.com>
2021-03-22 08:56:04 +0100

    Add files via upload

--- lib/templates/Connexion/Tools/rounded_input_field.dart
+++ lib/templates/Connexion/Tools/rounded_input_field.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:oficihome/templates/Compte/constants.dart';
 
+import 'package:oficihome/templates/widgets/constants.dart';
+
 import 'package:oficihome/templates/Connexion/Tools/text_field_container.dart';
 
 class RoundedInputField extends StatelessWidget {
@@ -8,9 +10,9 @@ class RoundedInputField extends StatelessWidget {
   final IconData icon;
   final ValueChanged<String> onChanged;
   const RoundedInputField({
-    Key key,
-    this.hintText,
-    this.icon = Icons.person,
+    Key key, 
+    this.hintText, 
+    this.icon = Icons.person, 
     this.onChanged,
   }) : super(key: key);
 
--- lib/templates/oficihome_app_theme.dart
+++ lib/templates/oficihome_app_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class OficihomeAppTheme {
   OficihomeAppTheme._();
   static const Color orangeFonce = Color(0xFFF54A19);
-  static const Color orange = Color(0xFFFF8C00);
+  static const Color orange = Color(0xFFf4a261);
   static const Color orangeClair = Color(0xFFFFAA66);
   static const Color orangeTresClair = Color(0xFFFAB29E);
   static const Color white = Color(0xFFFFFFFF);
