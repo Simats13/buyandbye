69293c151d3cd1f12fbbe8a5bfbd0b226612ac09
Simats13 <39007520+Simats13@users.noreply.github.com>
2021-08-31 18:32:57 +0200

    Enlever le false

--- lib/templates/pages/pageCompte.dart
+++ lib/templates/pages/pageCompte.dart
@@ -316,7 +316,7 @@ class _PageCompteState extends State<PageCompte> {
                                           CupertinoButton(
                                               child: Text('Annuler'),
                                               onPressed: () {
-                                                Navigator.of(context).pop(false);
+                                                Navigator.of(context).pop();
                                               }),
                                           CupertinoButton(
                                             child: Text('Déconnexion',
