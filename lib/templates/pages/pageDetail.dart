16b4f685708bf4a8995c9b1740c0fc0e978de83e
Mathis Maximin <maximinmathis@gmail.com>
2021-11-16 11:54:52 +0100

    push

--- lib/templates/pages/pageDetail.dart
+++ lib/templates/pages/pageDetail.dart
@@ -220,8 +220,8 @@ class _PageDetail extends State<PageDetail> with LocalNotificationView {
           return <Widget>[
             PreferredSize(
               preferredSize: const Size.fromHeight(10),
-              child: CupertinoSliverNavigationBarDetail(
-                automaticallyImplyTitle: false,
+              child: CupertinoNavigationBarDetail(
+                automaticallyImplyLeading: false,
                 leading: IconButton(
                   icon: Container(
                     width: 50,
@@ -268,7 +268,6 @@ class _PageDetail extends State<PageDetail> with LocalNotificationView {
                     // Navigator.pop(context);
                   },
                 ),
-                largeTitle: Text(""),
               ),
             ),
           ];
