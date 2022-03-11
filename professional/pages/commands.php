c70064c1abf47dfbaa1852987cd133b2656a2ee6
OnelonXClement <c.brunet@groupe-aen.info>
2022-03-11 11:07:53 +0100

    Affichage des commandes selon leur statut

--- professional/pages/commands.php
+++ professional/pages/commands.php
@@ -4,6 +4,56 @@ $uid = $_SESSION['verified_user_id'];
 $commands = $firestore->collection('commandes');
 $query = $commands->where('users', 'array-contains', $uid);
 $snapshot = $query->documents();
+
+function getCommands($snapshot, $firestore, $statut) {
+    foreach ($snapshot as $document) {
+        $ids = $document['users'][0] . $document['users'][1];
+
+        if ($statut == 0) {
+        // Récupère les commandes dont le statut est 0 (en attente)
+            $statut1 = $firestore->collection('commandes')->document($ids)->collection("commands");
+            $query1 = $statut1->where('statut', '=', 0);
+            $snapshot1 = $query1->documents();
+        } elseif ($statut == 1) {
+        // Récupère les commandes dont le statut est 1 (en cours)
+            $statut1 = $firestore->collection('commandes')->document($ids)->collection("commands");
+            $query1 = $statut1->where('statut', '=', 1);
+            $snapshot1 = $query1->documents();
+        } else {
+        // Récupère les commandes dont le statut est 2 (terminé)
+            $statut1 = $firestore->collection('commandes')->document($ids)->collection("commands");
+            $query1 = $statut1->where('statut', '=', 2);
+            $snapshot1 = $query1->documents();
+        }
+
+        foreach ($snapshot1 as $doc) {
+            ?>
+            <div class="parent">
+                <div class="left">
+                    <?php
+                    echo 'Date de commande : ' . date('d/m/Y à H:i', strtotime($doc['horodatage']));
+                    echo nl2br("\n");
+                    echo nl2br("\n");
+                    echo 'Référence de commande : ' . $doc['reference'];
+                    ?>
+                </div>
+                <div class="right">
+                    <?php
+                    if ($doc['articles'] == 1) {
+                        echo $doc['articles'] . ' article';
+                    } else {
+                        echo $doc['articles'] . ' articles';
+                    }
+                    echo nl2br("\n");
+                    echo nl2br("\n");
+                    echo 'Prix : ' . $doc['prix'] . '€';
+                    ?>
+                </div>
+            </div>
+            <?php
+        }
+    }
+}
 ?>
 
 <script>
@@ -47,14 +97,14 @@ $snapshot = $query->documents();
         border: none;
         color: white;
         text-align: center;
-        padding: 1% 4%;
-        font-size: 2vw;
+        padding: 0.8% 3%;
+        font-size: 1.2vw;
         border-radius: 8px;
         transition-duration: 0.4s;
     }
 
     button:hover {
-        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+        box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.24), 0 10px 16px rgba(0, 0, 0, 0.18);
     }
 
     button:focus {
@@ -72,39 +122,42 @@ $snapshot = $query->documents();
     #table3 {
         display: none;
     }
+
+    .card-body {
+        padding-left: 10vw;
+        padding-right: 10vw;
+    }
+
+    #buttons {
+        text-align: right;
+        padding-bottom: 2.5vh;
+    }
+
+    .parent {
+        clear: both;
+        overflow: hidden;
+        padding: 2vw;
+        border-radius: 25px;
+        margin-bottom: 3vw;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    }
+
+    .left {
+        float: left;
+    }
+
+    .right {
+        float: right;
+    }
 </style>
 
-<div id="page">
+<div id="page"> 
     <h1>Voir mes commandes</h1>
-    <?php
-    function getCommands($snapshot, $firestore, $statut) {
-        foreach ($snapshot as $document) {
-            $ids = $document['users'][0] . $document['users'][1];
-            // Récupère les commandes dont le statut est 0 (en attente)
-            $statut0 = $firestore->collection('commandes')->document($ids)->collection("commands");
-            $query0 = $statut0->where('statut', '=', 0);
-            $snapshot0 = $query0->documents();
-            // Récupère les commandes dont le statut est 1 (en cours)
-            $statut1 = $firestore->collection('commandes')->document($ids)->collection("commands");
-            $query1 = $statut1->where('statut', '=', 1);
-            $snapshot1 = $query1->documents();
-            // Récupère les commandes dont le statut est 2 (terminé)
-            $statut2 = $firestore->collection('commandes')->document($ids)->collection("commands");
-            $query2 = $statut2->where('statut', '=', 2);
-            $snapshot2 = $query2->documents();
-                foreach (${'snapshot' . $statut} as $doc) {
-                    printf('ID de commande : ' . $doc['id']);
-                    echo nl2br("\n");
-                }
-        }
-    }
-    ?>
-
-    <button id="button1" onclick="button1()">En attente</button>
-    <button id="button2" onclick="button2()">En cours</button>
-    <button id="button3" onclick="button3()">Terminées</button>
-    <br><br>
-
+    <div id="buttons">
+        <button id="button1" onclick="button1()" style="background-color: #359738">En attente</button>
+        <button id="button2" onclick="button2()">En cours</button>
+        <button id="button3" onclick="button3()">Terminées</button>
+    </div>
     <!-- Tableau des commandes en attente -->
     <div id="table1" class="card shadow mb-4">
         <div class="card-header py-3">
@@ -123,7 +176,7 @@ $snapshot = $query->documents();
         </div>
 
         <div class="card-body">
-        <?php getCommands($snapshot, $firestore, 1);?>
+            <?php getCommands($snapshot, $firestore, 1);?>
         </div>
     </div>
 
@@ -134,7 +187,9 @@ $snapshot = $query->documents();
         </div>
 
         <div class="card-body">
-        <?php getCommands($snapshot, $firestore, 2);?>
+            <?php getCommands($snapshot, $firestore, 2);?>
         </div>
     </div>
 </div>
+
+<!-- Données à afficher : 'horodatage', 'reference' | 'articles', 'prix'
