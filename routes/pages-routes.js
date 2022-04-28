14931c08a551e758ec5ae1931bc23d4a419f84d8
Mathis Maximin <maximinmathis@gmail.com>
2022-04-28 15:59:34 +0200

    push

--- routes/pages-routes.js
+++ routes/pages-routes.js
@@ -23,11 +23,15 @@ router.use(cookieParser());
  
  
 
-
-router.get('/', function (req, res) {  
+router.get('/', function (req, res) {
   const sessionCookie = req.cookies.session || "";
-  firebase.auth().verifySessionCookie(sessionCookie, true).then(() => {res.render("professional/pages/dashboard")}).catch((error)=>res.render("pages/login"))
+  firebase.auth().verifySessionCookie(sessionCookie, true).then(async (decodedToken) => {
+    const uid = decodedToken.uid;
+    var shopInfos = await axios.get(req.protocol + '://' + req.get('host')  + "/api/shops/" + uid);
+    res.render("professional/pages/dashboard",{shopInfos:shopInfos.data})
+  }).catch((error)=>res.render("pages/login"))
 });
+
 router.get('/inscription', registerView);
 router.get('/connexion', loginView);
 // router.get('/home', homeView);   
