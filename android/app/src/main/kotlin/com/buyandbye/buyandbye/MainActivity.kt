dc2b967e7cc396e5669bea81c9c44e2f3e7c28e0
Vague-a-bonder <h.rene@groupe-aen.info.com>
2022-07-03 22:32:56 +0200

    upload dossier android

---
+++ android/app/src/main/kotlin/com/buyandbye/buyandbye/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.buyandbye.buyandbye
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/buyandbye/off/buyandbye/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.buyandbye.off
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
