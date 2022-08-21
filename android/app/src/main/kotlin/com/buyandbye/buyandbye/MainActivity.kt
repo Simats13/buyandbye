5ccd17bed8d146f18ca65025e1b8cd24f3df51df
Simats13 <maximinmathis@gmail.com>
2022-08-21 23:28:43 +0200

    upload

---
+++ android/app/src/main/kotlin/com/buyandbye/buyandbye/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.buyandbye.buyandbye
+
+import io.flutter.embedding.android.FlutterActivity
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
